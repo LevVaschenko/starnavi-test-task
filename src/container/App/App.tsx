897abb1e8221af6553e27cdc81afab9dfa818b72
Lev <levvaschenko@gmail.com>
2023-07-14 21:16:26 -0700

    clean files and folders

---
+++ src/container/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CssBaseline } from '@mui/material'
+import Main from 'container/Main/Main'
+
+function App() {
+    return (
+        <>
+            <CssBaseline />
+            <Main />
+        </>
+    )
+}
+
+export default App
