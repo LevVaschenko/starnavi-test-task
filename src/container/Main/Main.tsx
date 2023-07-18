260f80310c5e0baefdf85e20f3489701847c4bf1
Lev <levvaschenko@gmail.com>
2023-07-18 00:01:46 -0700

    add start button, inline styles

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { Container } from '@mui/material'
-import './Main.scss'
 
 type Difficulty = {
     id: string
@@ -21,29 +20,41 @@ const Main = (props: Props) => {
     const [modes, setModes] = useState<ModeData[]>([])
     const [selectedDifficulty, setSelectedDifficulty] = useState<Difficulty | null>(null)
     const [blueSquares, setBlueSquares] = useState<number[]>([])
-
+    const [startClicked, setStartClicked] = useState(false)
 
     const handleSquareHover = (squareIndex: number) => {
-        if (!hoveredSquares.includes(squareIndex)) {
+        if (startClicked && !hoveredSquares.includes(squareIndex)) {
             setHoveredSquares([...hoveredSquares, squareIndex])
             setBlueSquares([...blueSquares, squareIndex])
         }
     }
 
-
     const handleSquareLeave = (squareIndex: number) => {
-        if (hoveredSquares.includes(squareIndex)) {
+        if (startClicked && hoveredSquares.includes(squareIndex)) {
             setHoveredSquares(hoveredSquares.filter((index) => index !== squareIndex))
+            setBlueSquares(blueSquares.filter((index) => index !== squareIndex))
         }
     }
 
-
     const handleFieldLeave = () => {
-        setHoveredSquares(hoveredSquares.filter((index) => hoveredSquares.includes(index)))
+        if (startClicked) {
+            const filledSquares = blueSquares.filter(squareIndex => hoveredSquares.includes(squareIndex))
+            setHoveredSquares([...hoveredSquares, ...filledSquares])
+        }
     }
 
-    const handleDifficultyChange = (difficulty: Difficulty) => {
-        setSelectedDifficulty(difficulty)
+    const handleDifficultyChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedMode = modes.find((mode) => mode.id === event.target.value)
+        if (selectedMode) {
+            setSelectedDifficulty(selectedMode)
+            setHoveredSquares([])
+            setBlueSquares([])
+            setStartClicked(false)
+        }
+    }
+
+    const handleStartClick = () => {
+        setStartClicked(true)
     }
 
     useEffect(() => {
@@ -61,26 +72,19 @@ const Main = (props: Props) => {
         fetchData()
     }, [])
 
-    useEffect(() => {
-        if (selectedDifficulty) {
-            setHoveredSquares([])
-        }
-    }, [selectedDifficulty])
-
     return (
         <>
-            <Container className="container">
+            <Container className="container" style={{ display: 'flex', alignItems: 'flex-start', height: '90vh' }}>
                 <div className="game-field">
-                    <div className="difficulties-box">
+                    <div
+                        className="settings-box"
+                        style={{ display: 'flex', alignItems: 'flex-start' }}
+                    >
                         <select
-                        className='select'
+                            className="select"
                             value={selectedDifficulty?.id || ''}
-                            onChange={(e) => {
-                                const selectedMode = modes.find((mode) => mode.id === e.target.value)
-                                if (selectedMode) {
-                                    handleDifficultyChange(selectedMode)
-                                }
-                            }}
+                            onChange={handleDifficultyChange}
+                            style={{ marginTop: '50px', marginBottom: '50px', width: '140px', height: '30px' }}
                         >
                             {modes.map((mode) => (
                                 <option key={mode.id} value={mode.id}>
@@ -88,48 +92,65 @@ const Main = (props: Props) => {
                                 </option>
                             ))}
                         </select>
+                        {!startClicked && (
+                            <div className="start-container" style={{ marginTop: '50px', marginBottom: '50px', marginLeft: '15px'}}>
+                                <button className='start-button' style={{ width: '80px', height: '30px', color: 'white', backgroundColor: 'rgb(12, 93, 207)', border: 'none', borderRadius: '3px', cursor: 'pointer' }} onClick={handleStartClick}>
+                                    START
+                                </button>
+                            </div>
+                        )}
                     </div>
-                    <div className="field" onMouseLeave={handleFieldLeave}>
-                        {selectedDifficulty && Array(selectedDifficulty.field)
-                            .fill(null)
-                            .map((_, row) => (
-                                <div key={row} className="row">
-                                    {Array(selectedDifficulty.field)
-                                        .fill(null)
-                                        .map((_, col) => {
-                                            const squareIndex = row * selectedDifficulty.field + col
-                                            const isHovered = hoveredSquares.includes(squareIndex)
-                                            const squareClass = `square ${isHovered ? 'blue' : ''}`
-                                            return (
-                                                <div
-                                                    key={col}
-                                                    className={squareClass}
-                                                    onMouseEnter={() => handleSquareHover(squareIndex)}
-                                                    onMouseLeave={() => handleSquareLeave(squareIndex)}
-                                                ></div>
-                                            )
-                                        })}
-                                </div>
-                            ))}
-                    </div>
+                    {startClicked && selectedDifficulty && (
+                        <div className="field" onMouseLeave={handleFieldLeave} style={{ marginTop: '30px', display: 'flex' }}>
+                            {Array(selectedDifficulty.field)
+                                .fill(null)
+                                .map((_, row) => (
+                                    <div key={row} className="row">
+                                        {Array(selectedDifficulty.field)
+                                            .fill(null)
+                                            .map((_, col) => {
+                                                const squareIndex = row * selectedDifficulty.field + col
+                                                const isHovered = hoveredSquares.includes(squareIndex)
+                                                const squareClass = `square ${isHovered ? 'blue' : ''}`
+                                                const uniqueSquareKey = `square-${row}-${col}`
+                                                return (
+                                                    <div
+                                                        key={uniqueSquareKey}
+                                                        className={squareClass}
+                                                        onMouseEnter={() => handleSquareHover(squareIndex)}
+                                                        onMouseLeave={() => handleSquareLeave(squareIndex)}
+                                                        style={{ width: '50px', height: '50px', border: '1.5px solid black', cursor: 'pointer', order: isHovered ? 1 : 0, backgroundColor: isHovered ? 'rgb(12, 93, 207)' : 'transparent' }}
+                                                    ></div>
+                                                )
+                                            })}
+                                    </div>
+                                ))}
+                        </div>
+                    )}
                 </div>
-                <div className="hover-squares">
-                    <div className="hover-squares-text">
-                        <h2>Hover squares</h2>
-                    </div>
-                    <div className="hover-squares-details">
-                        {selectedDifficulty &&
-                            blueSquares.map((squareIndex) => {
+                {startClicked && selectedDifficulty && (
+                    <div className="hover-squares" style={{ marginLeft: '65px' }}>
+                        <div>
+                            <h2 className="hover-squares-text" style={{ marginTop: '50px', marginBottom: '20px'}}>Hover squares</h2>
+                        </div>
+                        <div className="hover-squares-details" style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                            {blueSquares.map((squareIndex, index) => {
                                 const row = Math.floor(squareIndex / selectedDifficulty.field) + 1
                                 const col = (squareIndex % selectedDifficulty.field) + 1
+                                const uniqueHoverSquareKey = `hover-square-${row}-${col}-${index}`
                                 return (
-                                    <div key={squareIndex} className='hover-square'>
+                                    <div
+                                        key={uniqueHoverSquareKey}
+                                        className="hover-square"
+                                        style={{ width: '175px', display: 'flex', color: 'rgba(123, 96, 52)', backgroundColor: 'rgba(251, 247, 220)', padding: '8px', fontWeight: 'bold', marginBottom: '2.5px' }}
+                                    >
                                         row {row} col {col}
                                     </div>
                                 )
                             })}
+                        </div>
                     </div>
-                </div>
+                )}
             </Container>
         </>
     )
